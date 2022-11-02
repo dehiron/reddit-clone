87a46678f94a0fba7a85752c7b4d502fce535f67
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-11-02 13:34:31 +0900

    React Typescript Intro / Create Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+const Layout:React.FC<LayoutProps> = ({ children } ) => {
+    return(
+        <>
+            {/* <Navbar /> */}
+            <main>{children}</main>
+        </>
+    )
+}
+export default Layout;
