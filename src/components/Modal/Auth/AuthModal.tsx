fdb97a6c5764da1d8a191851580c224e44ce4303
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-11-03 13:54:21 +0900

    useAuthState firebase hook (currently signed-in user, modify navbar UI for authenticated user)

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,15 +1,17 @@
 import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
 import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase/clientApp';
 
 
 const AuthModal:React.FC = () => {
 
     const [modalState, setModalState] = useRecoilState(authModalState)
+    const [user, loading, error] = useAuthState(auth);
 
     const handleClose = () => {
         setModalState((prev) => ({
@@ -17,6 +19,11 @@ const AuthModal:React.FC = () => {
             open: false,
         }));
     };
+
+    useEffect(() => {
+        if (user) handleClose()
+        console.log("user", user);
+    },[user])
 
     return(
         <>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,13 @@ import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/clientApp';
 
 const Navbar:React.FC = () => {
+
+    const [user, loading, error] = useAuthState(auth); //どのコンポーネントでも使える
+
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
             <Flex align="center">
@@ -14,7 +19,7 @@ const Navbar:React.FC = () => {
             </Flex>
             <SearchInput />
             {/* <Directory /> */}
-            <RightContent />
+            <RightContent user={user} />
         </Flex>
     )
 }
