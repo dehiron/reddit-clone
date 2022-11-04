c3de48e52c0f53baffa43c07dbfe6f6181175813
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-11-04 16:03:57 +0900

    Community Data Recoil Atom (global communities state)

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -9,3 +9,26 @@ export interface Community {
     createdAt?: Timestamp; //シリアライゼーションのエラーが出る要因になることも
     imageURL?: string;
 }
+
+interface CommunitySnippet {
+    communityId: string;
+    isModerator?: boolean;
+    imageURL?: string;
+}
+
+interface CommunityState {
+    mySnippets: CommunitySnippet[]
+    //visitedCommunities
+}
+
+const defaultCommunityState: CommunityState = {
+    mySnippets: [],
+}
+
+export const communityState = atom<CommunityState>({
+    key: "communitiesState",
+    default: defaultCommunityState,
+})
+
+
+
