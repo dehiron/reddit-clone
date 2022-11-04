d89f0e17a8770eeedebda9055536948e6dd143db
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-11-04 21:08:41 +0900

    Fetch User Community Snippets (getMyCommunities function)

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -8,8 +8,9 @@ import { IoSparkles } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg"; 
 import { MdOutlineLogin } from "react-icons/md"
 import { auth } from '../../../firebase/clientApp';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import { communityState } from '../../../atoms/communitiesAtom';
 
 
 
@@ -19,7 +20,14 @@ type UserMenuProps = {
 
 const UserMenu:React.FC<UserMenuProps> = ({ user }) => {
 
+    const resetCommunityState = useResetRecoilState(communityState); //recoild内のステートをリセットするフック
     const setAuthModalState = useSetRecoilState(authModalState);
+
+    const logout = async () => {
+        await signOut(auth);
+        // clear community state
+        resetCommunityState();
+    }
     
     return (
         <Menu>
@@ -80,7 +88,7 @@ const UserMenu:React.FC<UserMenuProps> = ({ user }) => {
                             fontSize="10pt"
                             fontWeight={700}
                             _hover={{ bg: "blue.500", color: "white" }}
-                            onClick={() => {signOut(auth)}}
+                            onClick={logout}
                         >
                             <Flex align="center">
                                 <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
